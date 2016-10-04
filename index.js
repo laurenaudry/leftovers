6e045fb7ab7da76bbbaccd4efccc4399acbd2894
Lauren Johnson <laurenaudry1995@gmail.com>
2016-10-04 11:23:26 -0500

    changed index js to work hopefully

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Main from './lib/main'
+var express = require('express');
+var app = express();
 
-render((
-  <Main name="leftovers"/>
-), document.getElementById('app'))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
