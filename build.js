b461d30ae061808cf22c63392a523a476ac2cf62
Lauren Johnson <laurenaudry1995@gmail.com>
2016-10-17 15:05:22 -0500

    added grocery list, might remove... we'll see

--- build.js
+++ build.js
@@ -3,10 +3,12 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import Main from './lib/main'
 import Pantry from './lib/pantry'
+import Grocery from './lib/grocery.js'
 
 render((
   <Router history={ hashHistory }>
     <Route path="/" component={ Main }/>
     <Route path="/pantry" component={ Pantry }/>
+    <Route path="/grocery" component={ Grocery }/>
   </Router>
 ), document.getElementById('app'))
